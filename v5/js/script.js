ef3ca3d942aa2b0fb561ba51b64455baedb7189b
aman-maharshi <amanm4harshi@gmail.com>
2020-05-17 21:46:58 +0530

    typing effect cursor change

--- v5/js/script.js
+++ v5/js/script.js
@@ -1,18 +1,3 @@
-
-// setTimeout(function() {
-//     document.querySelector('.preloader-overlay').style.display = 'none';
-// }, 1250)
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.querySelector('.preloader-overlay').style.display = 'none';
-// })
-
-window.addEventListener('load', () => {
-    document.querySelector('.preloader-overlay').style.display = 'none';
-});
-
-
 /*
     Change navbar on scroll
 ------------------------------*/
@@ -36,3 +21,63 @@ window.addEventListener('scroll', () => {
         }) 
     }
 })
+
+/*
+    Typing Effect in the Header
+---------------------------------*/
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+
+const textArray = ['Web Developer', 'Programmer', 'Musician'];
+const typingDelay = 100;
+const erasingDelay = 70;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if(!(cursorSpan.classList.contains('typing'))) {
+        cursorSpan.classList.add('typing');
+    }
+    // if last letter of a word is not typed
+    if(charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        cursorSpan.classList.remove('typing');
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if(!(cursorSpan.classList.contains('typing'))) {
+        cursorSpan.classList.add('typing');
+    }
+    // if string is not entirely erased
+    if(charIndex > 0) { 
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        cursorSpan.classList.remove('typing');
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) {
+            textArrayIndex = 0;
+        }
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+
+// Hiding Preloader and Calling the Typing function
+window.addEventListener('load', () => {
+    document.querySelector('.preloader-overlay').style.display = 'none';
+    if(textArray.length) {
+        setTimeout(type, newTextDelay + 250);
+    }
+});
+
+
