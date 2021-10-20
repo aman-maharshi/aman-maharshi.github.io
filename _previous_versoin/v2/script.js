1971c304efd5c20350d7c58d31f9c7e70068a78e
aman-maharshi <amanm4harshi@gmail.com>
2021-10-20 16:50:53 +0530

    filered and rearranged previous versions

--- _previous_versoin/v2/script.js
+++ _previous_versoin/v2/script.js
@@ -2,10 +2,9 @@
 //     document.querySelector('.preloader-overlay').style.display = 'none';
 // }, 1250)
 
-
 // document.addEventListener('DOMContentLoaded', () => {
 //     document.querySelector('.preloader-overlay').style.display = 'none';
 // })
-window.addEventListener('load', () => {
-    document.querySelector('.preloader-overlay').style.display = 'none';
-});
+window.addEventListener("load", () => {
+    document.querySelector(".preloader-overlay").style.display = "none"
+})
